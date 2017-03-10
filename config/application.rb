84812003c6800c96da834ea9607486d1beba2450
rodinux <rodolphe.robles@sfr.fr>
2017-03-10 04:28:43 +0100

    try to debug

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,9 @@ module Demande
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.initialize_on_precompile = false
     config.autoload_paths += %W(#{config.root}/lib)
+    config.eager_load_paths += %W(#{config.root}/lib)
     config.assets.paths << Rails.root.join('vendor', 'assets', "*")
 
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.

20bcf802c7ba59f9fe70cfe1339757b1aabaad7c
rodinux <rodolphe.robles@sfr.fr>
2018-03-23 02:00:52 +0100

    update rails and gems

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+ActiveSupport.halt_callback_chains_on_return_false = false
 
 module Demande
   class Application < Rails::Application
@@ -28,6 +29,6 @@ module Demande
     config.i18n.default_locale = :fr
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
